3bb357ed841bb5544608493dc29c66c10839c707
Bhavana <kopparapubhavana@gmail.com>
2019-02-07 15:58:42 +0530

    Implemented the code for storing data in database

--- mysql.py
+++ mysql.py
@@ -1,29 +1,34 @@
 import sqlite3
-from flask import Flask
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
-def mysql():
-    #Connecting to the database
-    con = sqlite3.connect('examples')
-    print("openned database successfully!")
+@app.route("/")
+def render():
+    return render_template('mysql.html')
 
-    #Creating cursor 
-    cur = con.cursor()
 
-    #Creating a table
-    con.execute("CREATE TABLE details(name varchar(30), phone varchar(10), city varchar(30))")
-    print("Table created successfully!")
-    
-    #Inserting the values to the database
-    con.execute("INSERT INTO details VALUES('Bhavana', '8951534306', 'Bangalore')")
-    con.execute("INSERT INTO details VALUES('Harshita', '9740026935', 'Bangalore')")
+@app.route("/mysql", methods=["GET", "POST"])
+def ajax():
+    if request.method == 'POST':
+        name = request.form['username']
+        phone = request.form['phone']
+        city = request.form['city']
 
-    #Fetching the data from the table
-    cur.execute("SELECT * FROM details")
-    rows = cur.fetchall()
-    print(rows)
-    
-    #Closing the connection
-    con.close()
+        #Connecting to examples database
+        con = sqlite3.connect('examples')
+        print("openned database successfully!")
 
-mysql()
+        # Cursor to move around the details
+        cur = con.cursor()
+        con.execute("INSERT INTO details VALUES(?,?,?)",(name, phone, city))
+
+        #Fetching the details
+        cur.execute("SELECT * FROM details")
+        rows = cur.fetchall()
+        print(rows)
+        #Closing the connection
+        con.close()
+        return render_template('mysql.html', data = rows)
+
+if __name__ == '__main__':
+    app.run(port = 5001, debug = True)
